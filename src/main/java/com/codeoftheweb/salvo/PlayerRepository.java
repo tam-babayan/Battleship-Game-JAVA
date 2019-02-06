9b35d3437c1dec13679da345ba89a16a1742d688
tam-babayan <tam.babayan@gmail.com>
2019-02-06 11:13:29 +0100

    Added Spring Security library to the project

--- src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -1,12 +1,11 @@
 package com.codeoftheweb.salvo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    @Query("select u from #{#entityName} u where u.lastname = ?1")
-    Player findByUserName(String userName);
+//    @Query("select u from #{#entityName} u where u.lastname = ?1")
+//    Player findByUserName(String userName);
 
 }
