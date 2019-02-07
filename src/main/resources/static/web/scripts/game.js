405dcb54216272d22b2202e086e4a8db75ba47bd
tam-babayan <tam.babayan@gmail.com>
2019-02-07 22:24:59 +0100

    Refactored login/logout/signup

--- src/main/resources/static/web/scripts/game.js
+++ src/main/resources/static/web/scripts/game.js
@@ -1,82 +1,99 @@
 new Vue ({
-    el: '#app',
+    el: "#app",
     data: {
-        gameInfo: [],
-        ships: [],
-        salvoes: [],
-        rows: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-        columns: ['',1,2,3,4,5,6,7,8,9,10],
-        playerId: 1
+    gameInfo: [],
+    ships: [],
+    salvoes: [],
+    rows: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    columns: ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    playerId: 1
     },
+
     mounted() {
-        this.getgameInfoInfo()
+    this.getgameInfoInfo();
     },
+
     computed: {
-        board () {
-            let array = []
-            for (let i = 0; i < this.rows.length; i++) {
-                let object = {  rowId : this.rows[i],
-                                column : []
-                              }
-                for (let j = 1; j < this.columns.length; j++) {
-                    let anotherObject = {   columnId: this.columns[j],
-                                            ship: this.getShipCells(this.rows[i] + this.columns[j]),
-                                            mySalvoes: this.getMySalvoCell(this.rows[i] + this.columns[j]),
-                                            oponentSalvoes: this.getOponentSalvoes(this.rows[i] + this.columns[j]),
-                                            salvoTurn: this.getTurn(this.rows[i] + this.columns[j])
-                                        }
-                    object.column.push(anotherObject)
-                }
-                array.push(object)
-            }
-            console.log(array)
-            return array;
+    board() {
+      let array = [];
+      for (let i = 0; i < this.rows.length; i++) {
+        let object = { rowId: this.rows[i], column: [] };
+        for (let j = 1; j < this.columns.length; j++) {
+          let anotherObject = {
+            columnId: this.columns[j],
+            ship: this.getShipCells(this.rows[i] + this.columns[j]),
+            mySalvoes: this.getMySalvoCell(this.rows[i] + this.columns[j]),
+            oponentSalvoes: this.getOponentSalvoes(
+              this.rows[i] + this.columns[j]
+            ),
+            salvoTurn: this.getTurn(this.rows[i] + this.columns[j])
+          };
+          object.column.push(anotherObject);
         }
-    },
-    methods: {
-        getgameInfoInfo () {
-            axios
-                .get('http://localhost:8080/api/game_view/' + this.playerId)
-                .then(response => {
-                    this.gameInfo = response.data
-                    console.log(this.gameInfo)
-                    this.ships = this.gameInfo.ships
-                    this.salvoes = this.gameInfo.salvoes
-                    console.log(this.salvoes)
-                 })
-                .catch(error => console.log(error))
-        },
-        getShipCells(coordinate) {
-            for (let i = 0; i < this.ships.length; i++) {
-                if(this.ships[i].locations.includes(coordinate)) {
-                    return true
-                }
-            }
-            return false
-        },
-        getMySalvoCell(coordinate) {
-            for (let i = 0; i < this.salvoes.length; i++) {
-                if(this.salvoes[i].player === this.playerId && this.salvoes[i].locations.includes(coordinate)) {
-                    return true
-                }
-            }
-            return false
-        },
-        getOponentSalvoes(coordinate) {
-            for (let i = 0; i < this.salvoes.length; i++) {
-                if(this.salvoes[i].player != this.playerId && this.salvoes[i].locations.includes(coordinate)) {
-                    return true
-                }
-            }
-            return false
-        },
-        getTurn (coordinate) {
-            for (let i = 0; i < this.salvoes.length; i++) {
-                if (this.salvoes[i].locations.includes(coordinate) ) {
-                    return this.salvoes[i].turn
-                }
-            }
-            return null;
-        }
+        array.push(object);
+      }
+      return array;
     }
+    },
+
+    methods: {
+    getgameInfoInfo() {
+      axios
+        .get("http://localhost:8080/api/game_view/" + this.playerId)
+        .then(response => {
+          this.gameInfo = response.data;
+          this.ships = this.gameInfo.ships;
+          this.salvoes = this.gameInfo.salvoes;
+        })
+        .catch(error => console.log(error));
+    },
+    getShipCells(coordinate) {
+      for (let i = 0; i < this.ships.length; i++) {
+        if (this.ships[i].locations.includes(coordinate)) {
+          return true;
+        }
+      }
+      return false;
+    },
+    getMySalvoCell(coordinate) {
+      for (let i = 0; i < this.salvoes.length; i++) {
+        if (
+          this.salvoes[i].player === this.playerId &&
+          this.salvoes[i].locations.includes(coordinate)
+        ) {
+          return true;
+        }
+      }
+      return false;
+    },
+    getOponentSalvoes(coordinate) {
+      for (let i = 0; i < this.salvoes.length; i++) {
+        if (
+          this.salvoes[i].player != this.playerId &&
+          this.salvoes[i].locations.includes(coordinate)
+        ) {
+          return true;
+        }
+      }
+      return false;
+    },
+    getTurn(coordinate) {
+      for (let i = 0; i < this.salvoes.length; i++) {
+        if (this.salvoes[i].locations.includes(coordinate)) {
+          return this.salvoes[i].turn;
+        }
+      }
+      return null;
+    },
+    logOut() {
+      fetch("/api/logout", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }).then(window.location.replace("/web/games.html"));
+    }
+}
 })
--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,90 +1,126 @@
 new Vue ({
-    el: '#app',
+  el: '#app',
 
-    data: {
-        games: [],
-        leaderBoard: [],
-        currentPlayer: [],
-        loggedInPlayyer: ' '
+  data: {
+    games: [],
+    leaderBoard: [],
+    currentPlayer: {},
+    isLoggedIn: false,
+    username: "",
+    password: ""
+  },
 
-    },
+  mounted() {
+    this.getGameInfo();
+    this.getLeaderBoardInfo();
+  },
 
-    mounted() {
-        this.getGameInfo()
-        this.getLeaderBoardInfo()
-        this.getPlayerEmail()
-    },
-
-    computed: {
-        formattedGames() {
-            return this.games.map(one => {
-                one.date = moment(one.date).format('DD.MM.YYYY HH:mm');
-                return one
-            })
-        }
-    },
-
-    methods: {
-        getGameInfo() {
-            axios
-                .get("/api/games")
-                .then(response => {
-                    this.games = response.data.games
-                    this.currentPlayer = response.data.player
-//                    console.log(this.gamelist)
-                })
-                .catch(error => console.log(error))
-        },
-        getLeaderBoardInfo() {
-            axios
-                .get("/api/games/scores")
-                .then(response => {
-                    this.leaderBoard = response.data
-                    this.leaderBoard.sort((a, b) => (a.total < b.total) ? 1 : -1);
-//                    console.log(this.leaderBoard)
-                })
-                .catch(error => console.log(error))
-        },
-        logIn() {
-            const form = document.getElementById("logInForm")
-            fetch('/api/login', {
-                    credentials: 'include',
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: `userName=${ form["name"].value }&password=${ form["pwd"].value }`,
-                })
-                .then(location.reload())
-
-        },
-        getPlayerEmail() {
-            axios
-                .get('/api/games')
-                .then(response => {
-                    this.loggedInPlayyer = response.data.player.email
-                })
-                .catch(error => console.log(error))
-        },
-
-        alert() {
-            alert("LogIn Failed")
-        },
-
-        logOut() {
-            const form = document.getElementById("logInForm")
-                fetch('/api/logout', {
-                        credentials: 'include',
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        body: `userName=${ form["name"].value }&password=${ form["pwd"].value }`,
-                    })
-                    .then(location.reload())
-
-        }
+  computed: {
+    formattedGames() {
+      return this.games.map(one => {
+        one.date = moment(one.date).format("DD.MM.YYYY HH:mm");
+        return one;
+      });
     }
+  },
+
+  methods: {
+    getGameInfo() {
+      axios
+        .get("/api/games")
+        .then(response => {
+          this.games = response.data.games;
+          this.currentPlayer = response.data.player;
+          if (this.currentPlayer != null) {
+            this.isLoggedIn = true;
+          }
+        })
+        .catch(error => console.log(error));
+    },
+    getLeaderBoardInfo() {
+      axios
+        .get("/api/games/scores")
+        .then(response => {
+          this.leaderBoard = response.data;
+          this.leaderBoard.sort((a, b) => (a.total < b.total ? 1 : -1));
+        })
+        .catch(error => console.log(error));
+    },
+    handleFormSubmit(e) {
+      if (e.target.name == "login") {
+        this.logIn();
+      } else if (e.target.name == "signup") {
+        this.signUp();
+      }
+    },
+    logIn() {
+      fetch("/api/login", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+//        body: `userName=${ form["name"].value }&password=${ form["pwd"].value }`,
+        body: `userName=${this.username}&password=${this.password}`
+      }).then(response => {
+        if (response.status == 200) {
+          this.getGameInfo();
+          this.username = "";
+          this.password = "";
+        }
+      });
+    },
+
+    validEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+
+    logOut() {
+      const form = document.getElementById("logOutForm");
+      fetch("/api/logout", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }).then(response => {
+        this.isLoggedIn = false;
+      });
+    },
+
+    signUp() {
+      if (!this.validEmail(this.username)) {
+        alert(this.username + " is not valid email");
+        return;
+      }
+
+      if (!this.password) {
+        alert("password is empty");
+        return;
+      }
+      fetch("/api/players", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `userName=${this.username}&password=${this.password}`
+      }).then(response => {
+        switch (response.status) {
+          case 201:
+            this.getLeaderBoardInfo();
+            this.username = "";
+            this.password = "";
+            break;
+          case 409:
+            alert(this.username + " already exists");
+            break;
+        }
+      });
+    }
+  }
 })
